803972789e626eef5e53e9932e3e457fbc0addc5
grejsi <gressa07@gmail.com>
2026-08-15 02:57:06 +0200

    Switch database to PostgreSQL

--- prisma/migrations/20260815005415_init/migration.sql
+++ prisma/migrations/20260815005415_init/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "FavoritePhoto" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "source" TEXT NOT NULL,
     "imageUrl" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT,
     "metadata" JSONB,
     "note" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "FavoritePhoto_pkey" PRIMARY KEY ("id")
 );
